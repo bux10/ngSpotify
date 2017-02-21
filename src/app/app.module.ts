5b966fa7c95bc12bc16063dee0331e4d2c0fab57
Dustin Moore <dmoore@frostbytecafe.com>
2017-02-21 14:53:44 -0700

    removed routes, not working right :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule,Routes} from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutComponent} from './components/about/about.component';
-import { SearchComponent } from './components/search/search.component';
+
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, RouterModule ],
-  declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
+  imports:      [ BrowserModule],
+  declarations: [ AppComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
